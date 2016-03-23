afba1162965444106ae0fa631c84c25951e6f000
kbose <kbose@172.26.71.42>
2016-03-23 17:49:20 -0500

    New Commit

---
+++ src/com/qcoe/config/StatsConstants.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.qcoe.config;
+
+/**
+ *
+ * @author kbose
+ */
+public class StatsConstants {
+    
+    public static String jn_STATISTICS_DETAIL_KEY ="statistics_detail_key";
+    public static String jn_SERVICE_NAME ="service_name";
+    public static String jn_END_TIME_STAMP ="end_time_stamp";
+    public static String jn_START_TIME_STAMP ="start_time_stamp";
+    public static String jn_SERVICE_TYPE ="service_type";
+    public static String jn_STATISTICS ="Statistics";
+    public static String jn_DETAILS ="Details";
+    
+    public static String jn_SERVER_NAME ="server_name";
+    public static String jn_HOSTNAME ="hostname";
+   
+    
+    
+    
+}
